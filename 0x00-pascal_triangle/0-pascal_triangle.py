bcac6917197b4986a87f14b74cfacbff3a4346b8
Temitope Olowosuyi <Temitope.Olowosuyi@etranzact.com>
2023-09-26 13:21:54 +0100

    added readme file

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,18 @@
 """This module solves pascal triangle using nested loops"""
 
-
 def pascal_triangle(n):
     """Pascal Triangle"""
     triangle = []
-    for x in range(n + 1):
-        currentRow = []
-        for y in range(x + 1):
-            if y == 0 or y == x:
-                currentRow.append(1)
-            else:
-                currentRow.append(triangle[x - 1][y] + triangle[x - 1][y -1])
-        triangle.append(currentRow)
-    return triangle
+    if n <= 0:
+        return [[]]
+    else:
+        for x in range(n):
+            currentRow = []
+            for y in range(x + 1):
+                if y == 0 or y == x:
+                    currentRow.append(1)
+                else:
+                    prevRow = triangle[x - 1] 
+                    currentRow.append(prevRow[y] + prevRow[y - 1]) 
+            triangle.append(currentRow)
+        return triangle
