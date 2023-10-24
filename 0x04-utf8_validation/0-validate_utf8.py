a3b5e830a1013c6e2971dc1f973d53f1fe0efdf8
Temitope Olowosuyi <Temitope.Olowosuyi@etranzact.com>
2023-10-24 22:19:19 +0100

    initial commit

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from typing import List
+
+
+def validUTF8(data: List[int]) -> bool:
+    """Determine if a given datda set represents a valid UTF-8 encoding"""
+    binary_sequence = ['10', '110', '1110', '11110']
+
+    if not data or any(not isinstance(int, x) for x in data):
+        return False
+
+    first_byte = bin(data[0])[2:]
+    """If only one element in a list"""
+    if len(data) == 1:
+        if len(first_byte) <= 8:
+            if any(first_byte.startswith(i) for i in binary_sequence):
+                return True
+        return False
+
+    """Convert list emements to binary and check their length"""
+    for i in data:
+        if len(bin(i)[2:]) > 8:
+            return False
+        if any(bin(i)[2:].startswith(j) for j in binary_sequence):
+            continue
+        else:
+            return False
+
+    return True
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,2 @@
+# 0x04. UTF-8 Validation
+This covers utf-8 encoding interview question
