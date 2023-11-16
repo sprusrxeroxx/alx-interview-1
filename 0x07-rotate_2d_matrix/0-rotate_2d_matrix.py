f7c36b6d9d90cb473f712450d17cf3e3222b5781
Temz <temitopeabiodun685@gmail.com>
2023-11-16 22:14:04 +0000

    initial commit

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""This modlue contains function the rotate
+2D matrix by 90 degrees"""
+
+
+def rotate_2d_matrix(matrix) -> None:
+    """Rotate 2D matrix by 90 degrees"""
+
+    for x in range(len(matrix)):
+        for y in range(x, len(matrix)):
+            # transpose matrix i.e swap elements across
+            # main diagonal (X - shaped swapping)
+            matrix[x][y], matrix[y][x] = matrix[y][x], matrix[x][y]
+
+    for x in range(len(matrix)):
+        matrix[x].reverse()
