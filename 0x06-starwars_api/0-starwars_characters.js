c9d5c30c9d0f928d38be19a9d4b33493c6c1491b
Temitope Olowosuyi <Temitope.Olowosuyi@etranzact.com>
2023-12-07 03:47:58 +0100

    resolved pending tasks

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -19,3 +19,36 @@ const printOrderly = (characters, index) => {
     }
   });
 };
+
+
+// const { argv } = require('process');
+// const request = require('axios');
+// const num = argv[2]
+// const baseUrl = `https://swapi-api.alx-tools.com/api/films/${num}`
+
+// const fetchFilmCharacters = (url, attributes) => {
+//   return new Promise((resolve, reject) => {
+//     request(url, (error, response, body) => {
+//       if (!error && response.statusCode == 200) {
+//         let charactersArr = JSON.parse(body);
+//         resolve(`${charactersArr}.${attributes}`);
+//       } else {
+//         reject(error);
+//       }
+//     })
+//   })
+// }
+
+// const printCharacters = async () => {
+//   try {
+//     const characters = await fetchFilmCharacters(baseUrl, 'characters');
+//     characters.forEach(async url => {
+//       const characterName = await fetchFilmCharacters(url, 'name');
+//       console.log(characterName);
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+  
+// }
+// printCharacters();
